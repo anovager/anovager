069ea889ea32635c30dcb7858b430dfe00497280
Anovager <163933716+anovager@users.noreply.github.com>
2024-11-25 16:25:14 -0300

    update: atualização completa do site

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
-// Mostra ou oculta o botão de retorno ao topo conforme o usuário rola a página
-window.onscroll = function () { scrollFunction() };
+let menuButton = document.querySelector("#menu-button")
+let listMenu = document.querySelector("#list-menu")
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scrollToTopBtn").style.display = "block";
-    } else {
-        document.getElementById("scrollToTopBtn").style.display = "none";
-    }
-}
-
-// Função para rolar suavemente para o topo da página quando o botão é clicado
-function topFunction() {
-    document.body.scrollTop = 0; // Para Safari
-    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
-}
-
-// Adiciona classe ao navbar quando a página é rolada para baixo
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 20) {
-        $('#navbar').addClass('navbar-scrolled');
-    } else {
-        $('#navbar').removeClass('navbar-scrolled');
-    }
-});
+menuButton.addEventListener('click', () => {
+  if (menuButton.className == 'closed') {
+    menuButton.className = 'opened'
+    listMenu.style.display = 'flex'
+  }else if (menuButton.className == 'opened') {
+    menuButton.className = 'closed'
+    listMenu.style.display = 'none'
+  }
+})
